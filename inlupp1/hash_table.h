702defbc421762c24075b60e2adda9e6ac2b595c
Johannes Segersten <johseger@gmail.com>
2023-09-20 08:49:12 +0200

    assignment1_step3

--- inlupp1/hash_table.h
+++ inlupp1/hash_table.h
@@ -1,42 +1,31 @@
 #pragma once
-
+#include <stdbool.h>
 /**
  * @file hash_table.h
- * @author write both your names here
- * @date 1 Sep 2022
- * @brief Simple hash table that maps integer keys to string values.
- *
- * Here typically goes a more extensive explanation of what the header
- * defines. Doxygens tags are words preceeded by either a backslash @\
- * or by an at symbol @@.
- *
- * @see $CANVAS_OBJECT_REFERENCE$/assignments/gb54499f3b7b264e3af3b68c756090f52
+ * @author Johannes Segersten, Samuel Sverker
+ * @date september 17 2023
+ * @brief simple hash table that maps integer keys to string values
  */
-
 typedef struct hash_table ioopm_hash_table_t;
-
 /// @brief Create a new hash table
-/// @return A new empty hash table
+/// @return a new empty hash table
 ioopm_hash_table_t *ioopm_hash_table_create(void);
-
-/// @brief Delete a hash table and free its memory
+/// @brief delete a hash table and free its memory
 /// @param ht a hash table to be deleted
 void ioopm_hash_table_destroy(ioopm_hash_table_t *ht);
-
 /// @brief add key => value entry in hash table ht
 /// @param ht hash table operated upon
 /// @param key key to insert
 /// @param value value to insert
 void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value);
-
 /// @brief lookup value for key in hash table ht
 /// @param ht hash table operated upon
-/// @param key key to lookup
-/// @return the value mapped to by key (FIXME: incomplete)
-char *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key);
-
-/// @brief remove any mapping from key to a value
+/// @param key the key to lookup
+/// @param result ** a pointer to a char * that is updated upon success
+/// @return bool true if key maps to a value, false otherwise
+bool ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key, char **result);
+/// @Brief remove any mapping from key to a value
 /// @param ht hash table operated upon
 /// @param key key to remove
-/// @return the value mapped to by key (FIXME: incomplete)
+/// @return the value mapped to by key
 char *ioopm_hash_table_remove(ioopm_hash_table_t *ht, int key);
--- inlupp1/hash_table_test.c
+++ inlupp1/hash_table_test.c
@@ -1,14 +1,5 @@
 #include <CUnit/Basic.h>
 #include "hash_table.h"
-
-void test_create_destroy()
-{
-   ioopm_hash_table_t *ht = ioopm_hash_table_create();
-   CU_ASSERT_PTR_NOT_NULL(ht);
-   ioopm_hash_table_destroy(ht);
-   CU_ASSERT_PTR_NULL(ht);
-}
-
 int init_suite(void) {
   // Change this function if you want to do something *before* you
   // run a test suite
@@ -21,6 +12,61 @@ int clean_suite(void) {
   return 0;
 }
 
+void test_create_destroy() {
+ ioopm_hash_table_t *ht = ioopm_hash_table_create();
+ CU_ASSERT_PTR_NOT_NULL(ht);
+ ioopm_hash_table_destroy(ht);
+}
+void test_insert_once() {
+ ioopm_hash_table_t *h = ioopm_hash_table_create(); //creating a new hash table
+ int k = 0;
+ char *v = "Hello, world!";
+ char *result = NULL;
+ bool success=ioopm_hash_table_lookup(h, k, &result); // key does not map to any value in h, thus false 
+ CU_ASSERT_PTR_NULL(result);
+ CU_ASSERT_FALSE(success);
+ ioopm_hash_table_insert(h, k, v);
+ success = ioopm_hash_table_lookup(h, k, &result);
+ CU_ASSERT_PTR_NOT_NULL(result);
+ CU_ASSERT_TRUE(success);
+ ioopm_hash_table_destroy(h);
+}
+void test_insert_three() {
+ ioopm_hash_table_t *h = ioopm_hash_table_create();
+ int k[3]={1, 18, 35};
+ char *v[3]={"bar", "foo", "baz"};
+ char *results[3] = {NULL, NULL, NULL};
+ bool v_lookup_1 = ioopm_hash_table_lookup(h, k[0], &results[0]);
+ bool v_lookup_2 = ioopm_hash_table_lookup(h, k[1], &results[1]);
+ bool v_lookup_3 = ioopm_hash_table_lookup(h, k[2], &results[2]);
+ CU_ASSERT_FALSE(v_lookup_1);
+ CU_ASSERT_FALSE(v_lookup_2);
+ CU_ASSERT_FALSE(v_lookup_3);
+ CU_ASSERT_PTR_NULL(results[0]);
+ CU_ASSERT_PTR_NULL(results[1]);
+ CU_ASSERT_PTR_NULL(results[2]);
+ ioopm_hash_table_insert(h, k[0], v[0]);
+ ioopm_hash_table_insert(h, k[1], v[1]);
+ ioopm_hash_table_insert(h, k[2], v[2]);
+ v_lookup_1 = ioopm_hash_table_lookup(h, k[0], &results[0]);
+ v_lookup_2 = ioopm_hash_table_lookup(h, k[1], &results[1]);
+ v_lookup_3 = ioopm_hash_table_lookup(h, k[2], &results[2]);
+ CU_ASSERT(v_lookup_1 && v_lookup_2 && v_lookup_3);
+ CU_ASSERT_PTR_NOT_NULL(results[0]);
+ CU_ASSERT_PTR_NOT_NULL(results[1]);
+ CU_ASSERT_PTR_NOT_NULL(results[2]);
+ ioopm_hash_table_destroy(h);
+}
+void test_lookup_empty() {
+ ioopm_hash_table_t *ht = ioopm_hash_table_create();
+ char *result = NULL;
+ for (int i = 0; i < 17; ++i) /// 17 is a bit magical
+ {
+  CU_ASSERT_FALSE(ioopm_hash_table_lookup(ht, i, &result));
+ }
+ CU_ASSERT_FALSE(ioopm_hash_table_lookup(ht, -1, &result));
+ ioopm_hash_table_destroy(ht);
+}
 int main() {
   // First we try to set up CUnit, and exit if we fail
   if (CU_initialize_registry() != CUE_SUCCESS)
@@ -28,7 +74,7 @@ int main() {
 
   // We then create an empty test suite and specify the name and
   // the init and cleanup functions
-  CU_pSuite my_test_suite = CU_add_suite("My awesome test suite", init_suite, clean_suite);
+  CU_pSuite my_test_suite = CU_add_suite("Test of hash table functions", init_suite, clean_suite);
   if (my_test_suite == NULL) {
       // If the test suite could not be added, tear down CUnit and exit
       CU_cleanup_registry();
@@ -41,8 +87,11 @@ int main() {
   // the test in question. If you want to add another test, just
   // copy a line below and change the information
   if (
-    (CU_add_test(my_test_suite, "Create hash table test", test_create_destroy) == NULL) ||
-    0
+    (CU_add_test(my_test_suite, "create and destroy", test_create_destroy) == NULL) ||
+    (CU_add_test(my_test_suite, "insert once", test_insert_once) == NULL) || 
+    (CU_add_test(my_test_suite, "several insertions into the same bucket", test_insert_three) == NULL) 
+    || (CU_add_test(my_test_suite, "test_lookup_empty", test_lookup_empty) == NULL) 
+    ||    0
   )
     {
       // If adding any of the tests fails, we tear down CUnit and exit
